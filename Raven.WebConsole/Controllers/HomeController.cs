4359cd16ba063f2eaefc816f36e390c7fa7bf2cc
Roy Razon <royrazon@gmail.com>
2012-05-09 13:14:36 +0300

    added initial Raven.WebConsole and Raven.WebConsole.Main

---
+++ Raven.WebConsole/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Raven.WebConsole.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Raven.WebConsole/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Raven.WebConsole.MvcApplication" Language="C#" %>
